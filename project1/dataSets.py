db48fcca1c9b0c37eca43bb0fa0bf0f306d77fb6
leehj <guswns971022@gmail.com>
2022-01-27 11:00:08 +0900

    Day 22-01-27

--- project1/dataSets.py
+++ project1/dataSets.py
@@ -1,8 +1,8 @@
 import numpy as np
-import pandas as pandas
+import pandas as pd
 
 # Datasets
-df = pandas.DataFrame({
+df = pd.DataFrame({
    'col1': ['Item0', 'Item0', 'Item1', 'Item1'],
    'col2': ['Gold', 'Bronze', 'Gold', 'Silver'],
    'col3': [1, 2, np.nan, 4]
